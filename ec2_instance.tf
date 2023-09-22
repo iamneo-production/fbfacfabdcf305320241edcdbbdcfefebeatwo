806b332ab06c64c42fa3caee97eaeff6fa2becbe
Iamneo <techadmin@iamneo.ai>
2023-09-22 09:32:22 +0000

    Backup Commit

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -5,3 +5,18 @@ terraform {
         }
     }
 }
+
+providers "aws" {
+    region = "ap-northeast-1"
+    access_key = "AKIA5PND2T2MYVC5KSIY"
+    secret_key = "T0pCYeSCfSpqWThsMb3w8YDmcpsKl3puIhHS+Vzr"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-0ed99df77a82560e6"
+    instance_type = "t2.micro"
+}
+
+output "public_ip" {
+    value = aws_instance.example.public_ip
+}
